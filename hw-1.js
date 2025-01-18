1f57e0ee400f4e53a69147c6602680ff45b0ca52
Veritas-lab <veritas91@list.ru>
2025-01-18 23:10:29 +0300

    Задание 3 выполнено в файле hw-1.js

--- hw-1.js
+++ hw-1.js
@@ -4,3 +4,5 @@ a = 20;
 alert (a);
 const release_year_of_the_first_iphone = 2007;
 alert (release_year_of_the_first_iphone);
+const name_of_the_creator_java_script_language = "Brendan Eich";
+alert (name_of_the_creator_java_script_language);
