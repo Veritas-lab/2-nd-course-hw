6ed0eb5e787eb74bff2a7607780472a67911d409
Veritas-lab <veritas91@list.ru>
2025-01-18 23:44:40 +0300

    Внесены правки в hw-1.js

--- hw-1.js
+++ hw-1.js
@@ -2,10 +2,13 @@ let a = 10;
 alert(a);
 a = 20;
 alert(a);
+
 const release_year_of_the_first_iphone = 2007;
 alert(release_year_of_the_first_iphone);
+
 const name_of_the_creator_java_script_language = "Brendan Eich";
 alert(name_of_the_creator_java_script_language);
+
 let num1 = 10;
 let num2 = 2;
 let sum = num1 + num2;
@@ -16,12 +19,15 @@ alert("Сумма: " + sum);
 alert("Разность: " + difference);
 alert("Произведение: " + product);
 alert("Частное: " + quotient);
+
 const result = Math.pow(2, 5);
 alert(result);
+
 let a1 = 9;
 let b = 2;
 let remainder = a1 % b;
 alert(remainder);
+
 let num = 1;
 num += 5;
 num -= 3;
@@ -30,13 +36,18 @@ num /= 3;
 num++;
 num--;
 alert(num);
+
 let age = promt("Сколько вам лет?");
 alert("Вам " + age + " лет.");
-const user = {
+
+{
+user = {
     name: "Иванов Иван Иванович",
     age: 33,
     isAdmin: false
+}
 };
+
 let userName;
 userName = prompt("Как вас зовут?");
 alert("Привет, " + userName + "!");
