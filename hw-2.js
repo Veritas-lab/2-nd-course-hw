7813e7c5723b944ab66adc7ff85b9609174da17a
Veritas-lab <veritas91@list.ru>
2025-01-19 16:39:52 +0300

    Задание 3 добавлено в файл hw-2.js

--- hw-2.js
+++ hw-2.js
@@ -58,11 +58,21 @@ if (userInput === password) {
     console.log ('Пароль введен неправильно')
 };
 
-let c = 2;
+let c = 5;
 if (c > 0 && c < 10) {
     console.log("Верно");
-} else {
+} 
+else {
     console.log("Неверно");
+};
+
+let d = 500;
+let e = 25;
+if (d > 100 || e > 100) {
+    console.log('Верно');
+}
+else {
+    console.log('Неверно'); 
 };
 
 
