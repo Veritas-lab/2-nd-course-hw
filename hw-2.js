bd70e3a626cc9353f865844a79d3f13cda7c95f6
Veritas-lab <veritas91@list.ru>
2025-01-19 14:33:07 +0300

    Добавлен файл hw-2.js

---
+++ hw-2.js
@@ -0,0 +1,51 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+const release_year_of_the_first_iphone = 2007;
+alert(release_year_of_the_first_iphone);
+
+const name_of_the_creator_java_script_language = "Brendan Eich";
+alert(name_of_the_creator_java_script_language);
+
+let num1 = 10;
+let num2 = 2;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+alert("Сумма: " + sum);
+alert("Разность: " + difference);
+alert("Произведение: " + product);
+alert("Частное: " + quotient);
+
+const result = Math.pow(2, 5);
+alert(result);
+
+let a1 = 9;
+let b = 2;
+let remainder = a1 % b;
+alert(remainder);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+alert("Ваш возраст: " + age);
+
+const user = {
+    name: "Иванов Иван Иванович",
+    age: 33,
+    isAdmin: false
+};
+
+let userName;
+userName = prompt("Как вас зовут?");
+alert("Привет, " + userName + "!");
